4b10b0ef13c995089b4372725a74932dbb0c9ff3
Igor Boiko <igorboiko@gmail.com>
2023-05-17 15:53:58 +0300

    add Components

---
+++ src/components/AppBar/AppBar.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+export const Wrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+`;
+export const Section = styled.header`
+  display: grid;
+  gap: 4px;
+`;
+export const Title = styled.header`
+  margin: 0;
+  font-size: 16px;
+`;
---
+++ src/components/Layout/Layout.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  margin: 0 auto;
+  font-size: x-large;
+  padding: 0 0.5rem;
+`;
